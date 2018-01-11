e3332df9fb2f1601a088a663de549167b9d4441e
powmonk <dan@Aldron.co.uk>
2018-01-11 11:31:57 +0000

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,15 @@
-function play(var sound){
-	var audio = document.getElementById(sound);
-	audio.play();
+var jazz  = document.getElementById('jazz');
+var rain  = document.getElementById('rain');
+var noise = document.getElementById('noise');
+
+
+
+function playJazz(){
+	jazz.play();
+}
+function playRain(){
+	rain.play();
+}
+function playJazz(){
+	noise.play();
 }
