b604cec294091a0184cfd18d47fc3331e47c60cb
powmonk <dan@Aldron.co.uk>
2018-01-11 18:03:58 +0000

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,22 +4,25 @@ var noiseAudio = document.getElementById("noise");
 var trainAudio = document.getElementById("train");
 
 function play(audioType){
-	
-	switch(audioType) {
-		case 'jazz':
-			audio.play() = audio.paused?true:false;
-			break;
-		case 'rain':
-			var audio = document.getElementById("rain");
-			break;
-		case 'noise':
-			var audio = document.getElementById("noise");
-			break;
-		case 'train':
-			var audio = document.getElementById("train");
-			audio.volume = 0.05;
-			break;
-	}
+	exec(audioType + Audio.play())
+	//switch(audioType) {
+	//		
+	//		
+	//		
+	//	case 'jazz':
+	//		audio.play() = audio.paused?true:false;
+	//		break;
+	//	case 'rain':
+	//		var audio = document.getElementById("rain");
+	//		break;
+	//	case 'noise':
+	//		var audio = document.getElementById("noise");
+	//		break;
+	//	case 'train':
+	//		var audio = document.getElementById("train");
+	//		audio.volume = 0.05;
+	//		break;
+	//}
 	
 	//if(audio.paused){
 	//	audio.play();
