5d543980c7e176c2ebbe67f7ca8e1fa51157d308
powmonk <dan@Aldron.co.uk>
2018-01-11 18:05:39 +0000

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,32 +1,24 @@
-var jazzAudio  = document.getElementById("jazz");
-var rainAudio  = document.getElementById("rain");
-var noiseAudio = document.getElementById("noise");
-var trainAudio = document.getElementById("train");
-
 function play(audioType){
-	exec(audioType + Audio.play())
-	//switch(audioType) {
-	//		
-	//		
-	//		
-	//	case 'jazz':
-	//		audio.play() = audio.paused?true:false;
-	//		break;
-	//	case 'rain':
-	//		var audio = document.getElementById("rain");
-	//		break;
-	//	case 'noise':
-	//		var audio = document.getElementById("noise");
-	//		break;
-	//	case 'train':
-	//		var audio = document.getElementById("train");
-	//		audio.volume = 0.05;
-	//		break;
-	//}
 	
-	//if(audio.paused){
-	//	audio.play();
-	//}else{
-	//	audio.pause();
-	//}
+	switch(audioType) {
+		case 'jazz':
+			var audio = document.getElementById("rain");
+			break;
+		case 'rain':
+			var audio = document.getElementById("rain");
+			break;
+		case 'noise':
+			var audio = document.getElementById("noise");
+			break;
+		case 'train':
+			var audio = document.getElementById("train");
+			audio.volume = 0.05;
+			break;
+	}
+	
+	if(audio.paused){
+		audio.play();
+	}else{
+		audio.pause();
+	}
 }
